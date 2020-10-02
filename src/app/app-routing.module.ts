2ee3387e0865527927b740dd80b78bc360305e91
felkiriinya <felkiriinya@gmail.com>
2020-10-03 01:07:17 +0300

    added and styled the home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MyProfileComponent } from './components/my-profile/my-profile.component';
+import { NameResultComponent } from './components/name-result/name-result.component';
+import { RepoResultComponent } from './components/repo-result/repo-result.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+    {path: '', redirectTo: 'home', pathMatch:'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'my-profile', component: MyProfileComponent},
+    {path: 'name-result/:username' , component:NameResultComponent},
+    {path: 'repo-result/:reponame', component:RepoResultComponent},
+    
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+
 import { MyProfileComponent } from './components/my-profile/my-profile.component';
 import { RepoResultComponent } from './components/repo-result/repo-result.component';
 import { NameResultComponent } from './components/name-result/name-result.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { VisitedRepoDirective } from './directives/visited-repo.directive';
 import { DaysPassedPipe } from './pipes/days-passed.pipe';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +26,8 @@ import { DaysPassedPipe } from './pipes/days-passed.pipe';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
