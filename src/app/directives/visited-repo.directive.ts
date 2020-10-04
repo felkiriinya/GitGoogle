b564ec65ac096ce1e08b0055697fdcab54f1e04f
felkiriinya <felkiriinya@gmail.com>
2020-10-04 20:07:56 +0300

    added the directive to change the color of a visited repo

--- src/app/directives/visited-repo.directive.ts
+++ src/app/directives/visited-repo.directive.ts
@@ -1,10 +1,18 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appVisitedRepo]'
 })
 export class VisitedRepoDirective {
 
-  constructor() { }
+    constructor(private elem:ElementRef) {}
+
+    @HostListener("click") onClicks(){
+      this.text("rgb(247, 84, 111")
+    }
+  
+    private text(action:string){ 
+      this.elem.nativeElement.style.color=action;
+    }
 
 }
